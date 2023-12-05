5c78c66d08bc8a9320f5385f23b89cb360ad1cb9
celik-muhammed <developerghost01@gmail.com>
2023-12-05 21:42:58 +0300

    Updated

---
+++ MLE-Zoomcamp-M10-Solution/pycode/q6_test.py
@@ -0,0 +1,15 @@
+import requests
+
+
+url = "http://localhost:9696/predict"
+client = {"job": "retired", "duration": 445, "poutcome": "success"}
+
+# response = requests.post(url, json=client).json()
+# print(response)
+
+
+from time import sleep
+while True:
+    sleep(0.1)
+    response = requests.post(url, json=client).json()
+    print(response)
